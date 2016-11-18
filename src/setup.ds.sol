98124ecd460470f4462f311618b71932b0775f14
Nikolai Mushegian <nikolai@nexusdev.us>
2016-11-18 04:09:52 -0500

    GNT in setup script, dappfile

---
+++ src/setup.ds.sol
@@ -0,0 +1,26 @@
+// From: https://gist.github.com/mhhf/803df998215871f65dc4ec7ee4ac21d2
+
+// transfer.ds.sol
+pragma solidity ^0.4.4;
+
+import "dapple/script.sol";
+import "dapple/test.sol";
+import "./golem_source.sol";
+
+contract SetupTokenBalances is Script {
+  function SetupTokenBalances() {
+
+    // Set up an Actor
+    var actor = new Tester();
+    actor._target(env.gnt);
+    export("actor", actor);
+
+    txoff();
+    // this should be treated as a static call
+    uint balance = env.gnt.balanceOf(tx.origin);
+    txon();
+
+    // give the actor some balance
+    env.gnt.transfer(actor, balance);
+  }
+}
