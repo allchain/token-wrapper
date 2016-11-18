4158bbc2b2d901ae37748d857f73b48f5003c086
Nikolai Mushegian <nikolai@nexusdev.us>
2016-11-18 12:00:14 -0500

    make the deploy scripts build

--- src/deploy/wrap_gnt.ds.sol
+++ src/deploy/wrap_gnt.ds.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.4;
 
+import 'dapple/script.sol';
 import 'dapple/env.sol';
 import './interfaces.sol';
 
-contract DeployGNTWrapper is DappleEnv {
+contract DeployGNTWrapper is Script {
     function DeployGNTWrapper() {
         var wrapper = new TokenWrapper(ReducedToken(address(env.gnt)));
         export("GNT_wrapper", wrapper);
--- src/deploy/wrap_mock_token.ds.sol
+++ src/deploy/wrap_mock_token.ds.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.4.4;
 
 import 'erc20/erc20.sol';
 import 'dapple/env.sol';
+import 'dapple/script.sol';
 import './interfaces.sol';
 
-contract DeployMockTokenAndWrapper is DappleEnv {
-    function DeployMockTokenAndWrapper {
+contract DeployMockTokenAndWrapper is Script {
+    function DeployMockTokenAndWrapper() {
         var token = new ERC20Base(100 * 10**18);
         var wrapper = new TokenWrapper(ReducedToken(address(token)));
         export("mock_token", token);
-        export("mock_token_wrapper", mock_wrapper);
+        export("mock_token_wrapper", wrapper);
     }
 }
